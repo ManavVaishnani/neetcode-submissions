7503da816377cdd07e32841b04f226b1fdb43d58
Manav <72490321+ManavVaishnani@users.noreply.github.com>
2026-07-03 22:22:26 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        vector<int> count(3);
+        for(int& num : nums)
+            count[num]++;
+        int index = 0;
+        for(int i = 0; i<3; i++){
+            while(count[i]--)
+                nums[index++] = i;
+        }
+    }
+};
